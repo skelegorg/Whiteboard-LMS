ec020285841986fa4f4dd5891222327eac6a839f
skelegorg <62708878+skelegorg@users.noreply.github.com>
2021-01-10 22:16:48 -0500

    building poll.cs

--- WhiteboardAPI/WhiteboardAPI/Models/Assignments/Poll.cs
+++ WhiteboardAPI/WhiteboardAPI/Models/Assignments/Poll.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace WhiteboardAPI.Models.Assignments {
-    public class Poll {
-        //Poll class
-
-    }
+	public class Poll {
+		// This assignment type is a simple poll, any number of options up to 10
+		// can be anonymous or not (to teacher only- students can only see the number of choices on each option, 
+		// not who voted which
+		//
+	}
 }
